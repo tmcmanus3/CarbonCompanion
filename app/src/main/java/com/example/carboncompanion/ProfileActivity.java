c6e6c07892bd986318163f782279c22720644421
sacollins3 <sacollins3@wisc.edu>
2021-10-25 17:22:16 -0500

    Added profile activity

---
+++ app/src/main/java/com/example/carboncompanion/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.example.carboncompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    //TO DO
+    //set text for goal
+    //set text for username
+}
