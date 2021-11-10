04213fca33c2b0c601384e05d64365c867cab563
sacollins3 <sacollins3@wisc.edu>
2021-11-09 18:22:53 -0600

    Add activity UI and skeleton java file

--- app/src/main/java/com/example/carboncompanion/AddActivity.java
+++ app/src/main/java/com/example/carboncompanion/AddActivity.java
@@ -9,12 +9,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class AddActivity extends AppCompatActivity {
 
     private NavigationBarView bottomNavigationView;
-    private Button walkBtn;
+    private FloatingActionButton walkBtn;
+    private FloatingActionButton bikeBtn;
+    private FloatingActionButton busBtn;
+    private FloatingActionButton treeBtn;
+    private FloatingActionButton vegBtn;
+    private FloatingActionButton recycleBtn;
+    private FloatingActionButton lightBtn;
+    private FloatingActionButton volunteerBtn;
+    private FloatingActionButton localBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,72 @@ public class AddActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), WalkActivity.class));
+            }
+        });
+
+        //button to log a bike ride
+        bikeBtn = findViewById(R.id.bike);
+        bikeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(getApplicationContext(), BikeActivity.class));
+            }
+        });
+        //button to log a bus ride
+        busBtn = findViewById(R.id.bus);
+        busBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(getApplicationContext(), BusActivity.class));
+            }
+        });
+        //button to log planting a tree
+        treeBtn = findViewById(R.id.plant_tree);
+        treeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(getApplicationContext(), TreeActivity.class));
+            }
+        });
+
+        //button to log a vegetarian/vegan meal
+        vegBtn = findViewById(R.id.vegetarian_meal);
+        vegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(getApplicationContext(), VegActivity.class));
+            }
+        });
+        //button to log recycling
+        recycleBtn = findViewById(R.id.recycle);
+        recycleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(getApplicationContext(), RecycleActivity.class));
+            }
+        });
+        //button to log turning off the lights
+        lightBtn = findViewById(R.id.lights_off);
+        lightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(getApplicationContext(), LightActivity.class));
+            }
+        });
+        //button to log volunteering
+        volunteerBtn = findViewById(R.id.volunteer);
+        volunteerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(getApplicationContext(), VolunteerActivity.class));
+            }
+        });
+        //button to log shopping locally
+        localBtn = findViewById(R.id.shop_local);
+        localBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(getApplicationContext(), LocalActivity.class));
             }
         });
     }
